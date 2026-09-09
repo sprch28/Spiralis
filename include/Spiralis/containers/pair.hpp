80b2259d6aa4beb8a9a76f93fc524d1cd2e6f66b
sprch28 <spurchard28@gmail.com>
2026-09-09 16:41:20 -0700

    Better pair: Perfect forwarding

--- include/Spiralis/containers/pair.hpp
+++ include/Spiralis/containers/pair.hpp
@@ -4,21 +4,26 @@
 #include "../setup/init.hpp"
 #include <ostream>
 
-namespace sp{
-template <typename First, typename Second>
-class pair{
-public:
-    First first;
-    Second second;
-    constexpr pair() : first(), second(){}
-    constexpr pair(First one, Second two) : first(one), second(two){}
+namespace sp {
+
+template <typename T1, typename T2>
+struct pair {
+    T1 first;
+    T2 second;
+
+    constexpr pair() = default;
+
+    template <typename U1, typename U2>
+    constexpr pair(U1&& one, U2&& two) 
+        : first(sp::forward<U1>(one)), second(sp::forward<U2>(two)) {}
 };
+
+} // namespace sp
 
 template <typename First, typename Second>
 constexpr std::ostream& operator<<(std::ostream& os, const sp::pair<First, Second>& p) {
     os << "(" << p.first << ", " << p.second << ")";
     return os;
-}
 }
 
 #endif
