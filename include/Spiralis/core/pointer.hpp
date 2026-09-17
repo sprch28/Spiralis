a714abf53602c65c6db3e394a300424282155568
sprch28 <spurchard28@gmail.com>
2026-09-16 21:08:57 -0700

    shared_ptr and make_shared()

--- include/Spiralis/core/pointer.hpp
+++ include/Spiralis/core/pointer.hpp
@@ -8,6 +8,8 @@
 
 namespace sp {
 
+// control_block refactor coming soon in shared_ptr
+
 template <typename T, template <typename> typename Alloc = sp::allocator>
 class ptr{
 private:
@@ -17,11 +19,11 @@ private:
     constexpr ptr(T* data, Alloc<T> alloc) noexcept : ptr_(data), alloc_(sp::move(alloc)){}
     constexpr ptr(T* data) noexcept : ptr_(data), alloc_(){}
 
-    template <typename T, template<typename> typename Alloc, typename... Args>
-    friend constexpr ptr<T, Alloc> make_ptr(Alloc<T>, Args&&...);
+    template <typename U, template<typename> typename Allocator, typename... Args>
+    friend constexpr ptr<U, Allocator> make_ptr(Alloc<U>, Args&&...);
 
-    template<typename T, template<typename> typename Alloc, typename... Args>
-    friend constexpr ptr<T, Alloc> make_ptr(Args&&... args);
+    template<typename U, template<typename> typename Allocator, typename... Args>
+    friend constexpr ptr<U, Allocator> make_ptr(Args&&... args);
 
 public:
 
@@ -100,6 +102,161 @@ constexpr ptr<T, Alloc> make_ptr(Alloc<T> alloc, Args&&... args){
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+template<typename T, template<typename> typename Alloc = sp::allocator>
+class shared_ptr{
+public:
+    struct control_block {
+        private:
+
+        template <typename U, typename... Args>
+        static constexpr U* construct_at(U* location, Args&&... args) {
+            return ::new(static_cast<void*>(location)) U(sp::forward<Args>(args)...);
+        }
+
+        alignas(T) unsigned char storage[sizeof(T)];
+
+        public:
+
+        size_type strong_count = 1;
+        size_type weak_count = 0;
+
+        T* object() noexcept{
+            return reinterpret_cast<T*>(storage);
+        }
+
+        const T* object() const noexcept{
+            return reinterpret_cast<const T*>(storage);
+        }
+
+        template<typename... Args>
+        control_block(Args&&... args){
+            construct_at(
+                object(),
+                sp::forward<Args>(args)...
+            );
+        }
+    };
+private:
+
+    control_block* control_;
+    SP_NO_UNIQUE_ADDRESS Alloc<control_block> alloc_;
+    
+    constexpr shared_ptr(control_block* block, Alloc<control_block> alloc) : control_(block), alloc_(sp::move(alloc)){}
+    constexpr shared_ptr(control_block* block) : control_(block){}
+
+    template <typename U, template <typename> typename Allocator, typename... Args>
+    friend constexpr shared_ptr<U, Allocator> make_shared(Allocator<T> alloc, Args&&... args);
+
+    template <typename U, template <typename> typename Allocator, typename... Args>
+    friend constexpr shared_ptr<U, Allocator> make_shared(Args&&... args);
+
+public:
+
+    constexpr shared_ptr() noexcept : control_(nullptr) {}
+    constexpr shared_ptr(nullptr_t) noexcept : control_(nullptr) {}
+
+    constexpr shared_ptr(const shared_ptr& other) : control_(other.control_), alloc_(other.alloc_){
+        if(control_) ++control_->strong_count;
+    }
+    constexpr shared_ptr(shared_ptr&& other) noexcept : control_(other.control_), alloc_(sp::move(other.alloc_)){
+        other.control_ = nullptr;
+    }
+
+    constexpr shared_ptr& operator=(const shared_ptr& other){
+        SP_IF_NOT_EXPECT(this == &other) return *this;
+        reset();
+        control_ = other.control_;
+        alloc_ = other.alloc_;
+        if(control_) ++control_->strong_count;
+        return *this;
+    }
+    constexpr shared_ptr& operator=(shared_ptr&& other) noexcept{
+        SP_IF_NOT_EXPECT(this == &other) return *this;
+        reset();
+        control_ = other.control_;
+        alloc_ = sp::move(other.alloc_);
+        other.control_ = nullptr;
+        return *this;
+    }
+
+    SP_CONSTEXPR20 SP_FLATTEN ~shared_ptr() noexcept { reset(); }
+
+    constexpr T* get() noexcept { return control_ ? control_->object() : nullptr; }
+    constexpr const T* get() const noexcept { return control_ ? control_->object() : nullptr; }
+
+    constexpr Alloc<control_block>& get_allocator() noexcept { return alloc_; }
+    constexpr const Alloc<control_block>& get_allocator() const noexcept { return alloc_; }
+
+    constexpr T& operator*() noexcept { return *control_->object(); }
+    constexpr const T& operator*() const noexcept { return *control_->object(); }
+
+    constexpr T* operator->() noexcept{
+        return control_->object();
+    }
+    constexpr const T* operator->() const noexcept{
+        return control_->object();
+    }
+
+    constexpr explicit operator bool() const noexcept { return control_ != nullptr; }
+
+    constexpr size_type use_count() const noexcept { return control_ ? control_->strong_count : 0; }
+    constexpr bool unique() const noexcept { return control_ && control_->strong_count == 1; }
+
+    constexpr void reset() noexcept{
+        if(!control_) return;
+        if(--control_->strong_count == 0){
+            control_->object()->~T();
+            if(control_->weak_count == 0){
+                sp::allocator_traits<Alloc<control_block>>::destroy(
+                    alloc_,
+                    control_
+                );
+                sp::allocator_traits<Alloc<control_block>>::deallocate(
+                    alloc_,
+                    control_,
+                    1
+                );
+            }
+        }
+        control_ = nullptr;
+    }
+    constexpr void swap(shared_ptr& other) noexcept{
+        sp::swap(control_, other.control_);
+        sp::swap(alloc_, other.alloc_);
+    }
+
+};
+
+template<typename T, template<typename> typename Alloc = sp::allocator, typename... Args>
+constexpr shared_ptr<T, Alloc> make_shared(Args&&... args){
+    using block = typename shared_ptr<T, Alloc>::control_block;
+    Alloc<block> alloc;
+    block* memory =sp::allocator_traits<Alloc<block>>::allocate(alloc, 1);
+    sp::allocator_traits<Alloc<block>>::construct(
+        alloc,
+        memory,
+        sp::forward<Args>(args)...
+    );
+    return shared_ptr<T, Alloc>(
+        memory,
+        sp::move(alloc)
+    );
+}
+
+template<typename T, template<typename> typename Alloc = sp::allocator, typename... Args>
+constexpr shared_ptr<T, Alloc> make_shared(Alloc<typename shared_ptr<T, Alloc>::control_block> alloc, Args&&... args){
+    using block = typename shared_ptr<T, Alloc>::control_block;
+    block* memory =sp::allocator_traits<Alloc<block>>::allocate(alloc, 1);
+    sp::allocator_traits<Alloc<block>>::construct(
+        alloc,
+        memory,
+        sp::forward<Args>(args)...
+    );
+    return shared_ptr<T, Alloc>(
+        memory,
+        sp::move(alloc)
+    );
+}
 
 
 } // namespace sp
